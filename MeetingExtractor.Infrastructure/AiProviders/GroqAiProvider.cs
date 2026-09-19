c8aae51ea74ef1c4265169d5994edbf2bdb66e44
KhanhDo2604 <20521442@gm.uit.edu.vn>
2026-09-19 22:39:21 +1200

    feat:integrate Groq AI provider

---
+++ MeetingExtractor.Infrastructure/AiProviders/GroqAiProvider.cs
@@ -0,0 +1,55 @@
+﻿using MeetingExtractor.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.Text.Json;
+
+namespace MeetingExtractor.Infrastructure.AiProviders;
+
+public class GroqAiProvider: IAiProvider
+{
+    private readonly IHttpClientFactory _httpClientFactory;
+    private readonly IConfiguration _configuration;
+
+    public GroqAiProvider(IHttpClientFactory httpClientFactory, IConfiguration configuration)
+    {
+        _httpClientFactory = httpClientFactory;
+        _configuration = configuration;
+    }
+
+    public async Task<string> GenerateReplyAsync(string prompt)
+    {
+        var apiKey = _configuration["AiSettings:GroqApiKey"];
+        var client = _httpClientFactory.CreateClient();
+
+        var requestBody = new
+        {
+            model = "openai/gpt-oss-20b",
+            messages = new[]
+            {
+                new { role = "user", content = prompt }
+            }
+        };
+
+        var jsonContent = new StringContent(
+            JsonSerializer.Serialize(requestBody),
+            Encoding.UTF8,
+            "application/json");
+
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+        var response = await client.PostAsync(
+            "https://api.groq.com/openai/v1/chat/completions",
+            jsonContent);
+
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        using var document = JsonDocument.Parse(responseBody);
+        var messageContent = document.RootElement
+            .GetProperty("choices")[0]
+            .GetProperty("message")
+            .GetProperty("content")
+            .GetString();
+
+        return messageContent ?? string.Empty;
+    }
+}
--- MeetingExtractor.Infrastructure/DependencyInjection.cs
+++ MeetingExtractor.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,21 @@
-﻿namespace MeetingExtractor.Infrastructure
+﻿using MeetingExtractor.Application.Interfaces;
+using MeetingExtractor.Infrastructure.AiProviders;
+using MeetingExtractor.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MeetingExtractor.Infrastructure;
+public static class DependencyInjection
 {
-    public class DependencyInjection
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddHttpClient();
+        services.AddScoped<IAiProvider, GroqAiProvider>();
+
+        return services;
     }
 }
